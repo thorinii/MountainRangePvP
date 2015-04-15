8423cb63a7a767eb4f78d31f4eb398e8b1dc6319
ThorinII <flipsidelachy@gmail.com>
2015-04-15 16:35:15 +0930

    Extracted last pieces of Client/ServerGame into Game

--- src/main/java/mountainrangepvp/game/ServerGame.java
+++ src/main/java/mountainrangepvp/game/ServerGame.java
@@ -1,20 +1,8 @@
 package mountainrangepvp.game;
 
-import com.badlogic.gdx.Gdx;
-import mountainrangepvp.audio.AudioManager;
-import mountainrangepvp.input.InputHandler;
 import mountainrangepvp.mp.GameServer;
-import mountainrangepvp.mp.message.KillConnectionMessage;
-import mountainrangepvp.mp.message.Message;
-import mountainrangepvp.mp.message.NewWorldMessage;
-import mountainrangepvp.renderer.GameScreen;
 import mountainrangepvp.util.Log;
-import mountainrangepvp.world.physics.PhysicsSystem;
-import mountainrangepvp.world.terrain.HeightMap;
-import mountainrangepvp.world.terrain.HillsHeightMap;
-import mountainrangepvp.world.terrain.Terrain;
 
-import javax.swing.*;
 import java.io.IOException;
 
 /**
@@ -35,11 +23,7 @@ public class ServerGame extends Game {
         try {
             server = GameServer.startBasicServer(seed, world.isTeamModeOn());
         } catch (IOException ioe) {
-            Log.warn("Error starting server connection:", ioe);
-            JOptionPane.showMessageDialog(null, "Error starting server " + ioe,
-                                          "Mountain Range PvP",
-                                          JOptionPane.ERROR_MESSAGE);
-            Gdx.app.exit();
+            Log.crash("Could not start server", ioe);
         }
 
         super.create();
@@ -49,41 +33,5 @@ public class ServerGame extends Game {
     public void dispose() {
         super.dispose();
         server.stop();
-    }
-
-    @Override
-    public void accept(Message message, int id) throws IOException {
-        if (message instanceof KillConnectionMessage) {
-            KillConnectionMessage kill = (KillConnectionMessage) message;
-
-            Log.crash("Server disconnected: " + kill.getReason());
-        } else if (message instanceof NewWorldMessage) {
-            NewWorldMessage newWorldMessage = (NewWorldMessage) message;
-
-            Log.info("Received Seed", newWorldMessage.getSeed(), "Changing Map");
-
-            HeightMap heightMap;
-            switch (newWorldMessage.getWorldType()) {
-                case Hills:
-                    heightMap = new HillsHeightMap(newWorldMessage.getSeed());
-                    break;
-                default:
-                    heightMap = null;
-            }
-
-            world.setTerrain(new Terrain(heightMap));
-
-            physicsSystem = new PhysicsSystem(world);
-
-            inputHandler = new InputHandler(world);
-            inputHandler.register();
-
-            audioManager = new AudioManager(world.getPlayerManager(),
-                                            world.getShotManager(),
-                                            config);
-            audioManager.loadAudio();
-
-            gameScreen = new GameScreen(world);
-        }
     }
 }
