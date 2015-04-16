23b5429ad4daa483ff946efbebcab5daa4d0f698
ThorinII <flipsidelachy@gmail.com>
2015-04-16 19:44:27 +0930

    Split AudioManager between engine and game code

---
+++ src/main/java/mountainrangepvp/game/Sounds.java
@@ -0,0 +1,19 @@
+package mountainrangepvp.game;
+
+import mountainrangepvp.engine.SoundRef;
+
+/**
+ * Created by lachlan on 15/04/15.
+ */
+public class Sounds {
+    // Eventually parameterise this on gun class
+    public static SoundRef GunFire = new SoundRef("shot/fire.ogg");
+
+    public static SoundRef[] SOUNDS = {
+            GunFire
+    };
+
+    private Sounds() {
+        throw new UnsupportedOperationException("Must not be instantiated");
+    }
+}
