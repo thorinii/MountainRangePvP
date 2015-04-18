52e19811c7fdabc9db4bdff04e1570a8cb6a5dab
ThorinII <flipsidelachy@gmail.com>
2015-04-18 15:36:36 +0930

    Completed round trip

--- src/main/scala/mountainrangepvp/net/tcp/Message.scala
+++ src/main/scala/mountainrangepvp/net/tcp/Message.scala
@@ -1,5 +1,7 @@
 package mountainrangepvp.net.tcp
 
+import java.nio.charset.StandardCharsets
+
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import mountainrangepvp.engine.util.Log
@@ -13,10 +15,6 @@ case class LoginMessage(checkCode: Int, version: Int, nickname: String) extends 
 
 
 object Message {
-  def decode(buf: ByteBuf): Message = {
-    Log.info("decoded message")
-    ConnectedMessage(new ClientId(178293))
-  }
 
   def send(ctx: ChannelHandlerContext, msg: Message): Unit = {
     ctx.writeAndFlush(encode(ctx, msg)).sync
@@ -32,8 +30,46 @@ object Message {
   private def encode(msg: Message, buf: ByteBuf): Unit = msg match {
     case ConnectedMessage(id) =>
       buf.writeInt(1)
+      buf.writeLong(id.id)
+
+    case LoginMessage(checkCode, version, nickname) =>
+      buf.writeInt(2)
+      buf.writeInt(checkCode)
+      buf.writeInt(version)
+      writeString(buf, nickname)
 
     case _ =>
-      Log.todo()
+      Log.todoCrash()
+      throw new UnsupportedOperationException
+  }
+
+  def decode(buf: ByteBuf): Message = {
+    val `type` = buf.readInt()
+    `type` match {
+      case 1 =>
+        ConnectedMessage(new ClientId(buf.readLong()))
+
+      case 2 =>
+        LoginMessage(buf.readInt(),
+                     buf.readInt(),
+                     readString(buf))
+
+      case _ =>
+        Log.todoCrash()
+        throw new UnsupportedOperationException
+    }
+  }
+
+  private def writeString(buf: ByteBuf, string: String): Unit = {
+    val bytes = string.getBytes(StandardCharsets.UTF_8)
+    buf.writeInt(bytes.length)
+    buf.writeBytes(bytes)
+  }
+
+  private def readString(buf: ByteBuf): String = {
+    val length = buf.readInt()
+    val bytes = new Array[Byte](length)
+    buf.readBytes(bytes, 0, length)
+    new String(bytes)
   }
 }
--- src/main/scala/mountainrangepvp/net/tcp/ServerSideMessageHandler.scala
+++ src/main/scala/mountainrangepvp/net/tcp/ServerSideMessageHandler.scala
@@ -2,6 +2,7 @@ package mountainrangepvp.net.tcp
 
 import io.netty.buffer.ByteBuf
 import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
+import io.netty.util.AttributeKey
 import mountainrangepvp.engine.util.Log
 import mountainrangepvp.net.{ClientId, ClientInterface, ServerInterface}
 
@@ -17,21 +18,33 @@ class ServerSideMessageHandler(server: ServerInterface) extends SimpleChannelInb
   @throws(classOf[Exception])
   protected def channelRead0(ctx: ChannelHandlerContext, buf: ByteBuf) {
     val m = Message.decode(buf)
-    handle(m)
+    handle(idOf(ctx), m)
   }
 
-  private def handle(m: Message) = m match {
+  private def handle(client: ClientId, m: Message) = m match {
+    case LoginMessage(c, v, n) => server.login(client, c, v, n)
     case _ => Log.todo(m.toString)
   }
 
-  override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
-    Log.crash("Error in ServerSideMessageHandler", cause)
-    super.exceptionCaught(ctx, cause)
+  override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = cause match {
+    case e: Exception =>
+      Log.crash("Error in ServerSideMessageHandler", cause)
+      super.exceptionCaught(ctx, cause)
+
+    case _ =>
   }
+
+
+  private val idAttrKey = AttributeKey.valueOf[ClientId]("id")
+
+  private def idOf(ctx: ChannelHandlerContext): ClientId = ctx.attr(idAttrKey).get
 
   private class TcpClientInterface(ctx: ChannelHandlerContext) extends ClientInterface {
     override def connected(id: ClientId): Unit = {
+      ctx.attr(idAttrKey).set(id)
+
       Message.send(ctx, ConnectedMessage(id))
     }
   }
+
 }
