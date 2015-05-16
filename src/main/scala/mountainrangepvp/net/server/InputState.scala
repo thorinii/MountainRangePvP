263c5b9d1a79d6c5a69e2e78de30b177afad30b2
ThorinII <flipsidelachy@gmail.com>
2015-05-16 13:01:39 +0930

    Added fire ratelimiting

--- src/main/scala/mountainrangepvp/net/server/InputState.scala
+++ src/main/scala/mountainrangepvp/net/server/InputState.scala
@@ -7,16 +7,26 @@ import mountainrangepvp.game.world.InputCommand
  * Computes input state for each frame from an accumulated series of commands.
  */
 object InputState {
-  def apply(): InputState = new InputState(false, new Vector2(0, 0), 0)
+  def apply(): InputState = new InputState(false, 0, new Vector2(0, 0), 0)
+
+  val FireRateDelay = 0.1f
 }
 
 
-case class InputState(fire: Boolean, aimDirection: Vector2, acc: Int) {
-  def accumulate(command: InputCommand): InputState = {
-    new InputState(command.fire,
-                   if (acc == 0) command.aimDirection else aimDirection,
-                   acc + 1)
-  }
+case class InputState(private val fire: Boolean,
+                      private val fireTimer: Float,
+                      aimDirection: Vector2,
+                      private val acc: Int) {
+  def accumulate(command: InputCommand): InputState = copy(
+    fire = command.fire,
+    aimDirection = if (acc == 0) command.aimDirection else aimDirection,
+    acc = acc + 1)
 
-  def nextFrame = new InputState(false, aimDirection, 0)
+  def firing: Boolean = fire && fireTimer > InputState.FireRateDelay
+
+  def nextFrame(dt: Float) = copy(
+    fire = false,
+    fireTimer = if (firing) 0 else fireTimer + dt,
+    aimDirection = aimDirection,
+    acc = 0)
 }
