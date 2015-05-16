81ea5deb7fa2cb57fa15e5955fd098988f25b3e3
ThorinII <flipsidelachy@gmail.com>
2015-05-16 15:48:14 +0930

    Left, right, and jump keys are now sent to server

--- src/main/scala/mountainrangepvp/game/input/InputHandler.scala
+++ src/main/scala/mountainrangepvp/game/input/InputHandler.scala
@@ -10,6 +10,25 @@ class InputHandler(eventBus: EventBus, screenWidth: Int, screenHeight: Int) {
   private val inputMapper = new InputMapper
   private val inputProcessor = new StateAccumulatingInputProcessor(screenHeight)
 
+  def register() {
+    Gdx.input.setInputProcessor(inputProcessor)
+  }
+
+  def update(dt: Float): Unit = {
+    val rawState = inputProcessor.getState
+    val inputState = inputMapper.map(rawState)
+
+    val aim = rawState.mouse.cpy().sub(screenWidth / 2, screenHeight / 2).nor()
+    val run = (if (inputState("left")) -1 else 0) + (if (inputState("right")) 1 else 0)
+
+    eventBus.send(InputCommandEvent(
+      InputCommand(run = run,
+                   jump = inputState("jump"),
+                   fire = inputState("fire"),
+                   aimDirection = aim)))
+  }
+
+
   inputMapper.addState("fire")
   inputMapper.bindMouseButton(Bindings.MouseLeft, "fire")
 
@@ -21,28 +40,8 @@ class InputHandler(eventBus: EventBus, screenWidth: Int, screenHeight: Int) {
   inputMapper.bindKeyboard(Keys.RIGHT, "right")
   inputMapper.bindKeyboard(Keys.D, "right")
 
-  inputMapper.addState("down")
-  inputMapper.bindKeyboard(Keys.DOWN, "down")
-  inputMapper.bindKeyboard(Keys.S, "down")
-
   inputMapper.addState("jump")
   inputMapper.bindKeyboard(Keys.UP, "jump")
   inputMapper.bindKeyboard(Keys.W, "jump")
   inputMapper.bindKeyboard(Keys.SPACE, "jump")
-
-
-  def register() {
-    Gdx.input.setInputProcessor(inputProcessor)
-  }
-
-  def update(dt: Float): Unit = {
-    val rawState = inputProcessor.getState
-    val inputState = inputMapper.map(rawState)
-
-    val aim = rawState.mouse.cpy().sub(screenWidth / 2, screenHeight / 2).nor()
-
-    eventBus.send(InputCommandEvent(
-      InputCommand(fire = inputState("fire"),
-                   aimDirection = aim)))
-  }
 }
--- src/main/scala/mountainrangepvp/game/world/Events.scala
+++ src/main/scala/mountainrangepvp/game/world/Events.scala
@@ -16,5 +16,15 @@ case class InputCommandEvent(command: InputCommand) extends Event
 case object ServerDisconnect extends Event
 
 
-
-case class InputCommand(fire: Boolean, aimDirection: Vector2)
+/**
+ * A snapshot of the input system in game terminology.
+ * Run can be a decimal value (ie from a joystick), but will likely be ternary.
+ *
+ * @param run from -1 to 1, where -1 is left, 0 is no movement, and 1 is right.
+ * @param jump true if jumping.
+ * @param fire the status of the fire button.
+ * @param aimDirection the direction of aim.
+ */
+case class InputCommand(run: Float,
+                        jump: Boolean,
+                        fire: Boolean, aimDirection: Vector2)
